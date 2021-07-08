3685da76f3c4a176b16e4b01a729de315c40ea0b
Alejandro <d.alejandro198@gmail.com>
2021-07-08 16:39:33 -0500

    1.0

--- Bot.py
+++ Bot.py
@@ -1,19 +1,49 @@
 #este bot spamea mensajes por medio de la pantalla, se necesita tener instalsdo los modulos de pyautogui
 import pyautogui,time, keyboard
-time.sleep(10)
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import *
+from PyQt5.uic import *
+from PyQt5.QtCore import Qt
+import os
 
-while True:
-    pyautogui.typewrite("para que aprenda loka")
-    pyautogui.press("enter")
-    if keyboard.is_pressed("alt"):
-        print ("tecla a presionada")
-        break
-"""
-while True:
-    pyautogui.hotkey('ctrl','v')
-    time.sleep(0.5)
-    pyautogui.press("enter")
-    if keyboard.is_pressed("alt"):
-        print ("tecla a presionada")
-        break
- """
+def resource_path(relative_path):
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+class Bot(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        loadUi(resource_path("Bot.ui"), self)
+        self.botonActivar.clicked.connect(self.activar)
+        self.checkBoxImagen.clicked.connect(self.limpiar)
+    def limpiar(self):
+        self.textoInput.clear()
+    def activar(self):
+        if self.checkBoxImagen.isChecked():
+            time.sleep(10)
+            while True:
+                pyautogui.hotkey('ctrl','v')
+                time.sleep(0.5)
+                pyautogui.press("enter")
+                if keyboard.is_pressed("alt"):
+                    break
+        elif self.checkBoxTexto.isChecked():
+            time.sleep(10)
+            while True:
+                s = self.textoInput.text()
+                time.sleep(0.5)
+                pyautogui.typewrite(str(s))
+                pyautogui.press("enter")
+                if keyboard.is_pressed("alt"):
+                    break
+#main
+app = QApplication(sys.argv)
+bot = Bot()
+bot.show()
+app.exec_()
